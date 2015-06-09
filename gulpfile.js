690a3658eb104b509256fbd897483bf44205d926
Carl Henderson <chuckhendo@gmail.com>
2015-06-09 09:06:28 -0400

    Added release mechanism

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('sass', function() {
+    return gulp.src('*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(minifyCss({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('releases/'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('lib/bootstrap-sass-official/assets/javascripts/bootstrap.min.js')
+               .pipe(gulp.dest('releases/'));
+})
+
+
+gulp.task('build', ['sass', 'js']);
